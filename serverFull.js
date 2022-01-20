7adcf5b09790e224a79b7c9330d995e5ac3e6a43
Jerry <jerrywei998@gmail.com>
2022-01-19 20:40:10 -0500

    abcd

--- serverFull.js
+++ serverFull.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection({
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'Mima1234',
+        password: 'abcd1234',
         database: 'election'
     },
     console.log('Connected to the election database.')
